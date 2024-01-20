c66835be7faad4c76ce26f73ca119f7ab139d82b
muthu <kmuthukrishna@gmail.com>
2024-01-20 02:02:24 -0500

    finished REPL

--- src.cc
+++ src.cc
@@ -1,10 +1,75 @@
+#include <cstddef>
+#include <cstdlib>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+/*
+ * This function prints the prompt that the user is presented with
+ */
+void printPrompt() {
+    cout << "db > ";
+}
+
+/*
+ * InputBuffer is a wrapper around the buffer that is used for reading user input.
+ * @buffer : stores the user input
+ * @bufferLength: 
+ * @inputLength: 
+ */
+typedef struct {
+    string buffer;
+    size_t bufferLength;
+    ssize_t inputLength;
+}InputBuffer;
+
+/*
+ * This function returns a pointer to the wrapper object InputBuffer
+ * Initializes the buffer as an empty string, bufferLength and inputLength to 0
+ */
+InputBuffer* newInputBuffer() {
+    InputBuffer* inputBuffer = (InputBuffer*) malloc(sizeof(InputBuffer));
+    inputBuffer->buffer = "";
+    inputBuffer->bufferLength = 0;
+    inputBuffer->inputLength = 0;
+    return inputBuffer;
+}
+
+/*
+ * readInput function reads a line of input and stores the information accordingly into the inputBuffer
+ * @inputBuffer: Pointer the inputBuffer object which is used for storing the user input
+ * if user input is empty then the program exits
+ */
+void readInput(InputBuffer* inputBuffer) {
+    getline(cin, inputBuffer->buffer);
+    inputBuffer->inputLength = inputBuffer->buffer.size();
+    if (inputBuffer->inputLength <= 0) {
+        cout << "Error reading input\n";
+        exit(EXIT_FAILURE);
+    }
+}
+
+/*
+ * freeInputBuffer: frees in the inputBuffer object we created for storing the user input
+ */
+void freeInputBuffer(InputBuffer* inputBuffer) {
+    free(inputBuffer);
+}
+
 int main(int argc, char *argv[]) {
-    int b = pow(2, 4);
-    cout<<"hello world"; 
+    InputBuffer* inputBuffer = newInputBuffer();
+    while (true) {
+        printPrompt();
+        readInput(inputBuffer);
+        if (inputBuffer->buffer.compare(".exit") == 0) {
+            freeInputBuffer(inputBuffer);
+            exit(EXIT_SUCCESS);
+        }
+        else {
+            cout << "Unrecognized command "<<inputBuffer->buffer<<"\n";
+        }
+    }
     return 0;
 }
 
