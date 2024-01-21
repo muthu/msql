5bf99d2aa3681b70c82aa05fc5c9945580c87605
muthu <kmuthukrishna@gmail.com>
2024-01-20 21:01:09 -0500

    added select and insert statements functionality

--- src.cc
+++ src.cc
@@ -6,11 +6,30 @@
 using namespace std;
 
 /*
- * This function prints the prompt that the user is presented with
+ * This enum represents the state of meta command, meta commands are commands like .exit, .help etc
+ * @META_COMMAND_SUCCESS: this implies that the user input was indeed a meta command
  */
-void printPrompt() {
-    cout << "db > ";
-}
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+}metaCommandState;
+
+/*
+ * This enum represents the state of sql command, sql commands are like INSERT, SELECT etc
+ */
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+}prepareState;
+
+typedef enum {
+    STATEMENT_SELECT,
+    STATEMENT_INSERT
+}statementType;
+
+typedef struct {
+    statementType type;
+}statement;
 
 /*
  * InputBuffer is a wrapper around the buffer that is used for reading user input.
@@ -23,6 +42,13 @@ typedef struct {
     size_t bufferLength;
     ssize_t inputLength;
 }InputBuffer;
+
+/*
+ * This function prints the prompt that the user is presented with
+ */
+void printPrompt() {
+    cout << "db > ";
+}
 
 /*
  * This function returns a pointer to the wrapper object InputBuffer
@@ -57,18 +83,76 @@ void freeInputBuffer(InputBuffer* inputBuffer) {
     free(inputBuffer);
 }
 
+/*
+ * metaCommandState is a function that executes all the meta commands, and returns error response on unrecognized command
+ * @inputBuffer: the input buffer which stores the meta command
+ */
+metaCommandState doMetaCommand(InputBuffer* inputBuffer) {
+    if (inputBuffer->buffer.compare(".exit") == 0) {
+        freeInputBuffer(inputBuffer);
+        exit(EXIT_SUCCESS);
+    }
+    else {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+/*
+ * prepareStatement is a function that sets the type of sql statement that is being requested by the user
+ * @inputBuffer: buffer storing the user input 
+ * @statement: stores the type of statement being requested by the user
+ */
+prepareState prepareStatement(InputBuffer* inputBuffer, statement* statement) {
+    if (inputBuffer->buffer.compare(0, 6, "insert") == 0) {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (inputBuffer->buffer.compare(0, 6, "select") == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+    
+    return PREPARE_UNRECOGNIZED_STATEMENT; 
+}
+
+void executeStatement(statement* statement) {
+    switch (statement->type) {
+        case STATEMENT_SELECT:
+            cout << "This is where we would the execute the select statement\n";
+            break;
+        case STATEMENT_INSERT:
+            cout << "This is where we would the execute the insert statement\n";
+            break;
+    }
+}
+
 int main(int argc, char *argv[]) {
     InputBuffer* inputBuffer = newInputBuffer();
     while (true) {
         printPrompt();
         readInput(inputBuffer);
-        if (inputBuffer->buffer.compare(".exit") == 0) {
-            freeInputBuffer(inputBuffer);
-            exit(EXIT_SUCCESS);
+        if (inputBuffer->buffer[0] == '.') {
+            switch (doMetaCommand(inputBuffer)) {
+                case META_COMMAND_SUCCESS:
+                    continue;
+                case META_COMMAND_UNRECOGNIZED_COMMAND:
+                    cout << "Unrecognized command "<<inputBuffer->buffer<<"\n";
+                    continue;
+            }
         }
-        else {
-            cout << "Unrecognized command "<<inputBuffer->buffer<<"\n";
+
+        statement *statement;
+        switch (prepareStatement(inputBuffer, statement)) {
+            case PREPARE_SUCCESS:
+                break;
+            case PREPARE_UNRECOGNIZED_STATEMENT:
+                cout << "Unrecognized keyword at the start of "<<inputBuffer->buffer<<"\n";
+                continue;
+        
         }
+
+        executeStatement(statement);
+        cout << "Executed \n";
     }
     return 0;
 }
